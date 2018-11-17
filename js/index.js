074e06723fb051e785c9a7c02521837ca6640d07
Raymond Chow <raychow1944@yahoo.com>
2018-11-17 23:14:56 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
-let main = document.getElementById('main');
+const main = document.getElementById('main');
 
 main.style.color = 'red';
